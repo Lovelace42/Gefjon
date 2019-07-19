08361da4c22f631156e86944db81eeaf906755a1
Sophie Angus <sophiea@sfu.ca>
2019-07-19 16:20:43 -0700

    added Excercise Files and Project for API intro

---
+++ CityInfo/src/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityApp.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // Find City
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if(cityToReturn == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(cityToReturn);
+            }
+        }
+    }
+}
---
+++ CityInfo/src/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityApp.API.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            // Find City
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{pointId}")]
+        public IActionResult GetPointOfInterest(int cityId, int pointId)
+        {
+            // Find City
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // Find Point of Interest
+            var pointToReturn = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointId);
+            if (pointToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointToReturn);
+        }
+    }
+}
